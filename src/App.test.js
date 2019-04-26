8b6b10014ee28e3f2d3e9cf951caee474a32d51f
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-04-26 13:21:41 -0300

    Create jest tests for button and table

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search } from './App';
+import App, { Search, Table, Button } from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -31,4 +31,52 @@ describe('Search', () => {
   });
 });
 
+describe('Button', () => {
+  it('renders without crash', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>More</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('has a valid snapshot', () => {
+    const element = renderer.create(<Button>More</Button>);
+    const tree = element.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Table', () => {
+  const props = {
+    list: [
+      {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+      },
+      {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+      }
+    ]
+  };
+
+  it('renders wihtout crash', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+
+  });
+  it('has a valid snapshot', () => {
+    const element = renderer.create(<Table {...props} />);
+    const tree = element.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
 
